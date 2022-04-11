59877e8791fe75f91261a2ac67e9b532b38c4589
3oheicrw <93332305+3oheicrw@users.noreply.github.com>
2022-04-11 11:25:09 +0000

    Add files via upload

--- test.pas
+++ test.pas
@@ -1,6 +1,7 @@
-program test;
+﻿program test;
 {$mode objfpc}
-{$linklib libdw.a}
+{$apptype gui}
+
 uses Windows, dw;
 
 var
@@ -8,10 +9,9 @@ var
   flStyle: dword;
 begin
   dw_init(1, argc, argv);
-  writeln(ParamStr(0));
   flStyle:= DW_FCF_SYSMENU or DW_FCF_TITLEBAR or DW_FCF_TASKLIST or DW_FCF_DLGBORDER;
   mainwindow:= dw_window_new(HWND_DESKTOP, pchar('dwindows test UTF8 中国語 (繁体) cañón') , flStyle or DW_FCF_SIZEBORDER or DW_FCF_MINMAX);
-  dw_signal_connect(mainwindow, DW_SIGNAL_DELETE, @dw_main_quit, nil);
+  dw_signal_connect(mainwindow, DW_SIGNAL_DELETE, @dw_main_quit, @mainwindow);
   dw_window_set_size(mainwindow, 640, 550);
   dw_window_show(mainwindow);
   dw_main();
